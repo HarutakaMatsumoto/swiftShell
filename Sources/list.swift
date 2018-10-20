8c8ccbe0be3b146069880ac1020f7bdd2bba44fd
Harutaka Matsumoto <harutaka_matsumoto@icloud.com>
2018-10-20 23:46:50 +0900

    Revise list.swift

--- Sources/list.swift
+++ Sources/list.swift
@@ -1,1 +1,15 @@
 // This is the substitute of 'ls' command.
+import Foundation
+
+public func list(of directoryPath: String? = nil) {
+    
+    let resultPath = directoryPath ?? FileManager.default.currentDirectoryPath
+    
+    do {
+        var array = try FileManager.default.contentsOfDirectory(atPath: resultPath)
+        array.sort()
+        print(array.joined(separator: "\n"))
+    } catch {
+        print(error)
+    }
+}
