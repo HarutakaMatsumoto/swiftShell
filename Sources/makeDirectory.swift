a8bcf934c0235958fea6c706e1f97201573720b8
Harutaka Matsumoto <harutaka_matsumoto@icloud.com>
2018-10-24 10:59:58 +0900

    Revise makeDirectory.swift

--- Sources/makeDirectory.swift
+++ Sources/makeDirectory.swift
@@ -1,1 +1,15 @@
 // This is the substitute of 'mkdir' command.
+import Foundation
+
+public func makeDirectory(_ path: String) {
+    let manager = FileManager.default
+    do {
+        try manager.createDirectory(atPath: path, withIntermediateDirectories: false)
+        
+    } catch {
+        print(error)
+    }
+    
+}
+
+makeDirectory("Test")
